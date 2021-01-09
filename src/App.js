a87008aaf46068ce624200f738a3a45329f69b50
BosonHiggs-qt <19520611@gm.uit.edu.vn>
2021-01-09 14:12:32 +0700

    game oan tu ti

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+// import MainGame from './GameOanTuTI/MainGame';
+import BaiTapGame from './BaiTapGame1/BaiTapGame';
+// import BookingTicketFilm from './QuanLyVePhim/BookingTicketFilm';
+
+function App() {
+  return (
+    <div className="App">
+      <BaiTapGame/>
+      {/* <BookingTicketFilm/> */}
+    </div>
+  );
+}
+
+export default App;
